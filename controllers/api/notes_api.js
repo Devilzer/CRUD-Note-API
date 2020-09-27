6928cca0ae9be5f1aeabccf4bedd96cb098313b1
Deepak Jena <jenadeepak724@gmail.com>
2020-09-28 00:42:04 +0530

    added jwt authorization for CRUD features

--- controllers/api/notes_api.js
+++ controllers/api/notes_api.js
@@ -3,6 +3,8 @@ const Users = require("../../models/users");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
+let refreshTokens = [];
+
 module.exports.signUp = async (req, res) => {
   try {
     const user = new Users(req.body);
@@ -31,12 +33,51 @@ module.exports.signIn = (req, res) => {
           message: `ERROR Invalid username or password`,
         });
       }
-      const accessToken = jwt.sign(user);
+      const payload = {
+        username: user.username,
+        password: user.password,
+      };
+      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN, {
+        expiresIn: "10m",
+      });
+      const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN);
+      refreshTokens.push(refreshToken);
       return res.json(200, {
         message: `SignIn Successful Wellcome ${user.username}`,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
       });
     }
   );
+};
+
+module.exports.token = (req, res) => {
+  const token = req.body.token;
+  if (token == null) {
+    return res.status(401).json({
+      message: "Invalid request",
+    });
+  }
+  if (!refreshTokens.includes(token)) {
+    return res.status(403).json({
+      message: "Invalid request",
+    });
+  }
+  jwt.verify(token, process.env.REFRESH_TOKEN, (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        message: `error in token verification ${err}`,
+      });
+    }
+    const accessToken = jwt.sign(
+      { username: user.username, password: user.password },
+      process.env.ACCESS_TOKEN,
+      { expiresIn: "10m" }
+    );
+    return res.status(200).json({
+      newAccessToken: accessToken,
+    });
+  });
 };
 
 module.exports.createNote = async (req, res) => {
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const notesAPI = require("../../controllers/api/notes_api");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+router.use(express.json());
 
 router.post("/signup", notesAPI.signUp);
 router.post("/signin", notesAPI.signIn);
+router.post("/token", notesAPI.token);
 
 //CRUD (create read update delete routes)
-router.post("/create", notesAPI.createNote);
-router.get("/", notesAPI.showPosts);
-router.put("/update/:id", notesAPI.update);
-router.delete("/delete/:id", notesAPI.delete);
+router.post("/create", authenticateToken, notesAPI.createNote);
+router.get("/", authenticateToken, notesAPI.showPosts);
+router.put("/update/:id", authenticateToken, notesAPI.update);
+router.delete("/delete/:id", authenticateToken, notesAPI.delete);
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.status(401).json({
+      message: "Invalid request",
+    });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+    if (err) {
+      return res.status(403).json(err);
+    }
+    next();
+  });
+}
 module.exports = router;
