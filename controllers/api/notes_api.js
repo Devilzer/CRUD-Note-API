7915c399b57ada53046d5d3875ed60cda68f20b6
Deepak Jena <jenadeepak724@gmail.com>
2020-09-27 22:33:29 +0530

    Added user Authentication

--- controllers/api/notes_api.js
+++ controllers/api/notes_api.js
@@ -1,4 +1,40 @@
 const Notes = require("../../models/notes");
+const Users = require("../../models/users");
+
+module.exports.signUp = async (req, res) => {
+  try {
+    const user = new Users(req.body);
+    await user.save();
+    return res.status(200).json({
+      message: "User Created !",
+    });
+  } catch (error) {
+    return res.json(400, {
+      message: `error in creating User! ${error}`,
+    });
+  }
+};
+
+module.exports.signIn = (req, res) => {
+  Users.findOne(
+    { username: req.body.username, password: req.body.password },
+    (err, user) => {
+      if (err) {
+        return res.json(400, {
+          message: `ERROR in signing In`,
+        });
+      }
+      if (!user) {
+        return res.json(400, {
+          message: `ERROR Invalid username or password`,
+        });
+      }
+      return res.json(200, {
+        message: `SignIn Successful Wellcome ${user.username}`,
+      });
+    }
+  );
+};
 
 module.exports.createNote = async (req, res) => {
   try {
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const notesAPI = require("../../controllers/api/notes_api");
 const router = express.Router();
 
+router.post("/signup", notesAPI.signUp);
+router.post("/signin", notesAPI.signIn);
+
 //CRUD (create read update delete routes)
 router.post("/create", notesAPI.createNote);
 router.get("/", notesAPI.showPosts);
